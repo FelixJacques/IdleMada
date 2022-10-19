6f9c5954bb8493942dc29020d33f2aa282df45ad
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 07:05:01 -0400

    Update 9tatayeah.js

--- src/farm/9tatayeah.js
+++ src/farm/9tatayeah.js
@@ -2,10 +2,10 @@ class tatayeah {
   
   constructor() {
     this.name = "<:tatayeah:916814758655705128> La souris de tatayeah"
-    this.cps = "FA"
-    this.cost = "4C4B40"
+    this.cps = "320"
+    this.cost = "E4E1C0"
     this.farm = "tatayeah"
-    this.des = "TATAYEAAAAAAAAAAAAAAAH"
+    this.des = "*Pendant que certains sont hardstuck gold, Tatayet est parvenu à atteindre le top100 Overwatch avec 30 fps et cette souris antique à boule.*"
     this.img = "https://i.imgur.com/WoaZAgR.png"
   }
 }
