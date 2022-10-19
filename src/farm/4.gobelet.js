0488ffecab921e31f710237f8f972777d01f357c
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 01:52:29 -0400

    Update and rename 4.goblet.js to 4.gobelet.js

---
+++ src/farm/4.gobelet.js
@@ -0,0 +1,14 @@
+class gobelet {
+  
+  constructor() {
+    this.name = ":cup_with_straw: Le gobelet de Pappusu"
+    this.cps = "88B8"
+    this.cost = "218711A00"
+    this.farm = "aimbot"
+    this.des = "Pappusu adore crier dans son gobelet en plastique qui se trouve sur son bureau depuis 2 ans. On ne souhaite pas savoir ce qu’il fait le soir avec.
+"
+    this.img = "https://i.imgur.com/Slx3cMT.png"
+  }
+}
+
+module.exports = gobelet
