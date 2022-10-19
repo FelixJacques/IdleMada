f44688c0c0a2f29b386a07e8f2c1beb86d4e90f9
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 06:55:53 -0400

    Update 5eater.js

--- src/farm/5eater.js
+++ src/farm/5eater.js
@@ -2,10 +2,10 @@ class eater {
   
   constructor() {
     this.name = "<:pizzaklown:822192070479446056> Z-eater"
-    this.cps = "A"
-    this.cost = "7530"
+    this.cps = "1E"
+    this.cost = "4E20"
     this.farm = "eater"
-    this.des = "Le Z-eate, plus connu sous le nom de Zeklown, apprécie manger des fritures. "
+    this.des = "*Le Z-eater, plus connu sous le nom de Zeklown, apprécie manger des fritures.*"
     this.img = "https://i.imgur.com/qNevYFQ.png"
   }
 }
