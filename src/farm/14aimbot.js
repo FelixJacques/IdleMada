9fef5d022c81a714c85934b932a46ba722c7d1ae
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 01:47:40 -0400

    Update 14aimbot.js
    aimbot de livs

---
+++ src/farm/14aimbot.js
@@ -0,0 +1,13 @@
+class aimbot {
+  
+  constructor() {
+    this.name = "<:HitMark:307938386533023744> L'aimbot de Livs"
+    this.cps = "88B8"
+    this.cost = "218711A00"
+    this.farm = "aimbot"
+    this.des = "On sait pas comment il fait pour miss autant de shots. Toujours est-il qu'il mangera toutes vos grav même avec widow."
+    this.img = "https://i.imgur.com/oJEJANj.png"
+  }
+}
+
+module.exports = aimbot
