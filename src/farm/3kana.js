4c43d2eb8d849764875aaccd3c2aaaa456c326b0
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 06:49:53 -0400

    Update 3kana.js

--- src/farm/3kana.js
+++ src/farm/3kana.js
@@ -3,9 +3,9 @@ class kana {
   constructor() {
     this.name = "<:sheesh:1030749038133379102> Kanasheesh"
     this.cps = "5"
-    this.cost = "1388"
+    this.cost = "3E8"
     this.farm = "kana"
-    this.des = "Kana est le grand joueur du Casino Pokémon. Il vous versera peut-être une partie de sa fortune si l’envie lui passe par la tête."
+    this.des = "*Kana est le grand joueur du Casino Pokémon. Il vous versera peut-être une partie de sa fortune si l’envie lui passe par la tête.*"
     this.img = "https://i.imgur.com/4ebje5z.png"
   }
 }
