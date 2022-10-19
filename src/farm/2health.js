b54c9f86fb46ba03cb9e8e66318786dd634ee614
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 06:49:08 -0400

    Update 2health.js

--- src/farm/2health.js
+++ src/farm/2health.js
@@ -3,9 +3,9 @@ class health {
   constructor() {
     this.name = "<:healthpack:1030747047172780042> Healthpack"
     this.cps = "2"
-    this.cost = "1F4"
+    this.cost = "FA"
     this.farm = "health"
-    this.des = "Les healthpack sont censés soigner les blessures, mais ceux si sont des versions légèrement modifiées qui ont à la place soigner votre portefeuille."
+    this.des = "Les healthpack sont censés soigner les blessures, mais ceux si sont des versions légèrement modifiés qui vont à la place soigner votre portefeuille."
     this.img = "https://i.imgur.com/xNNM0O4.png"
   }
 }
