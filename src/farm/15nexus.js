227759993ff21183284e636766f0e0c75f8d1345
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 07:23:03 -0400

    Update 15nexus.js

--- src/farm/15nexus.js
+++ src/farm/15nexus.js
@@ -2,8 +2,8 @@ class nexus {
   
   constructor() {
     this.name = "🌐 Nexus"
-    this.cps = "186A0"
-    this.cost = "104C533C00"
+    this.cps = "30D40"
+    this.cost = "1D1A94A2000"
     this.farm = "nexus"
     this.des = "*Construction légendaire érigée par les ancêtres Pingoo, ce monument historique de Madacraft impressionne tous les historiens de part sa beauté mais aussi sa complexité. Le Nexus dans toute sa splandeur ici pendant 3 secondes : https://youtu.be/Cu9i9_5pwdw?t=922 *"
     this.img = "https://i.imgur.com/PvQV87o.png"
