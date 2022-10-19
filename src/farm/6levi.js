ff24c33753d60d01abbde51c7da083f939b81942
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 06:56:41 -0400

    Update 6levi.js

--- src/farm/6levi.js
+++ src/farm/6levi.js
@@ -2,7 +2,7 @@ class levi {
   
   constructor() {
     this.name = "<:LeviSmoll:887857690074837073> Smol Levi"
-    this.cps = "1E"
+    this.cps = "46"
     this.cost = "186A0"
     this.farm = "levi"
     this.des = "Une Pizzaklown est une pizza un peu particulière. Il faut beaucoup d’années d'entraînement pour apprendre à la manger correctement."
