93250103efaccd70a1049001d8d37c9a39c5aefc
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 07:12:17 -0400

    Update 13widow.js

--- src/farm/13widow.js
+++ src/farm/13widow.js
@@ -2,10 +2,10 @@ class widow {
   
   constructor() {
     this.name = "<:widowfat:1030751342244925450> La widow de Tak"
-    this.cps = "88B8"
-    this.cost = "218711A00"
+    this.cps = "7530"
+    this.cost = "6FC23AC00"
     this.farm = "widow"
-    this.des = "On sait pas comment il fait pour miss autant de shots. Toujours est-il qu'il mangera toutes vos grav même avec widow."
+    this.des = "*On sait pas comment il fait pour miss autant de shots. Toujours est-il qu'il mangera toutes vos grav, même avec widow.*"
     this.img = "https://i.imgur.com/p86xiTr.png"
   }
 }
