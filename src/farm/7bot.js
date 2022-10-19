8c6a7e714505689b3d5e378ab5a0aa338e3168bb
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 06:58:40 -0400

    Update 7bot.js

--- src/farm/7bot.js
+++ src/farm/7bot.js
@@ -1,11 +1,11 @@
 class bot {
   
   constructor() {
-    this.name = "📊 Bot Overstats"
-    this.cps = "50"
-    this.cost = "61A80"
+    this.name = ":robot: Bot Overstats"
+    this.cps = "96"
+    this.cost = "7A120"
     this.farm = "bot"
-    this.des = "Il observe toutes vos losestreaks dans l'ombre. Il vous apporte un peu d'argent à chaque game lose (source : tkt)"
+    this.des = "*Il observe toutes vos losestreaks dans l'ombre. Il vous apporte un peu d'argent à chaque game lose (source : tkt)*"
     this.img = "https://i.imgur.com/fnsYj1F.png"
   }
 }
