be505a0ec126215146e284c04ea114fd010d68f5
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 07:10:56 -0400

    Update 12belugod.js

--- src/farm/12belugod.js
+++ src/farm/12belugod.js
@@ -1,11 +1,11 @@
 class belugod {
   
   constructor() {
-    this.name = "<:Belugods:1030750809010491453> Belugods"
-    this.cps = "2EE0"
-    this.cost = "2926F965"
+    this.name = "<:Belugods:1030750809010491453> Sponsor Belugods"
+    this.cps = "2755"
+    this.cost = "12A05F200"
     this.farm = "belugod"
-    this.des = "Pour des raisons que l'on ne peut pas détailler ici, ce sponsor rapporte beaucoup d'argent."
+    this.des = "*Pour des raisons que l'on ne peut pas détailler ici, ce sponsor rapporte beaucoup d'argent. Sauf si y'a un ours jaune qui bouffe du miel dans la pièce.*"
     this.img = "https://i.imgur.com/3Or1dIp.png"
   }
 }
