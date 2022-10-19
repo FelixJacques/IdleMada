3f27834e4dd318b467c107267fa29646b2367b68
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 07:06:38 -0400

    Update 10aykicat.js

--- src/farm/10aykicat.js
+++ src/farm/10aykicat.js
@@ -2,10 +2,10 @@ class aykicat {
   
   constructor() {
     this.name = "<:Aykicat:430125201099849739> Aykicat"
-    this.cps = "3E8"
-    this.cost = "1C9C380"
+    this.cps = "7D0"
+    this.cost = "7BFA480"
     this.farm = "aykicat"
-    this.des = "Aykicat est tellement mignonne qu'elle vous apporte de l'argent rien qu'en vous regardant."
+    this.des = "*Aykicat est tellement mignonne qu'elle vous apporte de la richesse rien qu'en vous regardant.*"
     this.img = "https://i.imgur.com/YYS7TaR.png"
   }
 }
