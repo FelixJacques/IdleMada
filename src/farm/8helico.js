0af0c01222a5c85708fe2c24fac2a3c215fdd75b
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-19 07:02:10 -0400

    Update 8helico.js

--- src/farm/8helico.js
+++ src/farm/8helico.js
@@ -2,10 +2,10 @@ class helico {
   
   constructor() {
     this.name = ":helicopter: L'hélico de Pandoux"
-    this.cps = "1"
-    this.cost = "F4240"
+    this.cps = "12C"
+    this.cost = "2DC6C0"
     this.farm = "helico"
-    this.des = "L'hélico qui aura duré approximativement 12 minutes entre son achat et son crash dans une plage; il est l'investissement le moins rentable de l'histoire de GTA. Il est aujourd'hui classé en tant que patrimoine de MadaMada."
+    this.des = "*L'hélico qui aura duré approximativement 12 minutes entre son achat et son crash dans la mer; il est l'investissement le moins rentable de l'histoire de GTA 5. Il est aujourd'hui classé en tant que patrimoine de MadaMada. Origine : https://youtu.be/J7IJxdAujbk?t=52*"
     this.img = "https://i.imgur.com/MPpedJN.png"
   }
 }
