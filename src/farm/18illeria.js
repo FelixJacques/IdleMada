4b0be24a0a8b1e0d900d4072c4e2eac751a60327
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-20 00:24:18 -0400

    Update 18illeria.js

--- src/farm/18illeria.js
+++ src/farm/18illeria.js
@@ -1,1 +1,13 @@
+class illeria {
+  
+  constructor() {
+    this.name = "<:leviator:1032506121514131527> Farm à guardian d'Illeria"
+    this.cps = "2DC6C0"
+    this.cost = "38D7EA4C68000"
+    this.farm = "illeria"
+    this.des = "*Les anciens textes de MadaCraft indiquent que dans l'ancien monde Sanic world, une farm à guardian construite par une certaine Illéria aurait permis à l'économie de l'ancienne civilisation de ne pas s'effondrer, en apportant XP et ressources à tout le pays.*"
+    this.img = "https://i.imgur.com/jjOeEjz.png"
+  }
+}
 
+module.exports = illeria
