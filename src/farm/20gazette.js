0cfecd12f47c574d19e4dccdaea62c73133a4804
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-20 00:37:09 -0400

    Update 20gazette.js

--- src/farm/20gazette.js
+++ src/farm/20gazette.js
@@ -1,1 +1,13 @@
+class gazette {
+  
+  constructor() {
+    this.name = ":newspaper: Numéro de la Gazette"
+    this.cps = "17D7840"
+    this.cost = "11C37937E080000"
+    this.farm = "gazette"
+    this.des = "*La gazette du petit shimada se vend par dizaine de milliers d'exemplaires chaque semaine. Certains affirment que la Gazette cacherait parfois des codes secrets qui donnent des avantages ...*"
+    this.img = "https://i.imgur.com/vzqIdZw.png"
+  }
+}
 
+module.exports = gazette
