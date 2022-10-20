9a4b3799d37f196fb3dd6727228c080573c021a6
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-20 00:13:58 -0400

    Update and rename 17leviator to 17leviator.js

---
+++ src/farm/17leviator.js
@@ -0,0 +1,13 @@
+class leviator {
+  
+  constructor() {
+    this.name = "<:leviator:1032506121514131527> Carte légendaire Léviator"
+    this.cps = "124F80"
+    this.cost = "763BFBD22000"
+    this.farm = "leviator"
+    this.des = "*Une carte Pokémon ultra rare, que l'on obtient en trahissant ses camarades. Ça ne vous dit rien ?*"
+    this.img = "https://i.imgur.com/eKxYV4G.png"
+  }
+}
+
+module.exports = leviator
