c19905d44157a8da648733e6be1e0dc21a18816d
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-20 03:35:15 -0400

    Update 24papa.js

--- src/farm/24papa.js
+++ src/farm/24papa.js
@@ -1,1 +1,13 @@
+class papa {
+  
+  constructor() {
+    this.name = "<:papaGenji:1032557427658338344> Papa Genji"
+    this.cps = "77359400"
+    this.cost = "16C4ABBEBEA0100000"
+    this.farm = "papa"
+    this.des = "*Bonsoir*"
+    this.img = "https://i.imgur.com/CNVsHDh.png"
+  }
+}
 
+module.exports = papa
