5dccb227f2ba536c2b46399b49d0910ec1ccb57c
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-20 00:52:58 -0400

    Update 22casino.js

--- src/farm/22casino.js
+++ src/farm/22casino.js
@@ -1,1 +1,13 @@
+class casino {
+  
+  constructor() {
+    this.name = ":slot_machine: Casino Pokémon"
+    this.cps = "BEBC509"
+    this.cost = "53444835EC580000"
+    this.farm = "casino"
+    this.des = "*Vous êtes arrivés à un stade où la richesse vous permet des dingueries. Le Casino Pokémon, aka le casino le plus rentable au monde, n'est plus qu'un simple moyen d'investissement. Cependant, le gérant du Casino restera Jay-D pour toujours.*"
+    this.img = "https://i.imgur.com/LYnjTFh.png"
+  }
+}
 
+module.exports = casino
