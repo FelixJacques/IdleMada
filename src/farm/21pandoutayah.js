4040659e1724666060db8da9190ad2c092655f19
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-20 00:46:26 -0400

    Update 21pandoutayah.js

--- src/farm/21pandoutayah.js
+++ src/farm/21pandoutayah.js
@@ -1,1 +1,13 @@
+class pandoutayah {
+  
+  constructor() {
+    this.name = "<:Pandoux:307647799057711104> Les Lacs suspendus de Pandoutayah"
+    this.cps = "42C1D80"
+    this.cost = "9B6E64A8EC60000"
+    this.farm = "pandoutayah"
+    this.des = "*L’eau bénite qui coule dans son fleuve souterrain est si pure qu’elle abriterait l’origine de la vie des saniks.*"
+    this.img = "https://i.imgur.com/1vwHfPA.png"
+  }
+}
 
+module.exports = pandoutayah
