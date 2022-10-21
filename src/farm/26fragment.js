d2e85671ca823f2d6e5c225a3d6e0c7acdf0e593
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-20 21:21:34 -0400

    Update 26fragment.js

--- src/farm/26fragment.js
+++ src/farm/26fragment.js
@@ -1,1 +1,13 @@
+class fragment {
+  
+  constructor() {
+    this.name = ":diamond_shape_with_a_dot_inside: Fragment de matière condensée"
+    this.cps = "0"
+    this.cost = "152D02C7E14AF6800000"
+    this.farm = "fragment"
+    this.des = "*L'essence même de toute création. En posséder 10 vous permet de créer un item personnalisé.*"
+    this.img = "https://i.imgur.com/NV35ONV.png"
+  }
+}
 
+module.exports = fragment
