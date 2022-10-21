edc1cdb7151aa53b6daa1b20c3c7fb8bc71cc523
eijibm <66015359+eijibm@users.noreply.github.com>
2022-10-20 21:17:55 -0400

    Update 25vip.js

--- src/farm/25vip.js
+++ src/farm/25vip.js
@@ -1,1 +1,13 @@
+class vip {
+  
+  constructor() {
+    this.name = "💳 Pass VIP MadaMada"
+    this.cps = "2540BE400"
+    this.cost = "A2A15D09519BE00000"
+    this.farm = "vip"
+    this.des = "*Un pass prestigieux, qui vous garanti une boisson gratuite lors du prochain meeting MadaMada. Quel luxe!*"
+    this.img = "https://i.imgur.com/bXZRqwj.png"
+  }
+}
 
+module.exports = vip
